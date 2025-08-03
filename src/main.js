6cd171e4a6a2434f3872f7a6072a0a52a08262f2
gabriel <gabrielscarpino@hotmail.com>
2025-08-03 16:44:26 -0300

    Adicionando o faq sem resposta

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
 
 
     for (let i = 0; i < buttons.length; i++) {
@@ -12,9 +13,20 @@ document.addEventListener('DOMContentLoaded', function () {
             botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
 })
 
-function removeBotaoAtivo(){
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe)
+}
+
+function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
 
     for (let i = 0; i < buttons.length; i++) {
@@ -25,7 +37,7 @@ function removeBotaoAtivo(){
 function escondeTodasAbas() {
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
 
-    for (let i = 0; i < tabsContainer.length; i++){
+    for (let i = 0; i < tabsContainer.length; i++) {
         tabsContainer[i].classList.remove('shows__list--is-active');
     }
 }
